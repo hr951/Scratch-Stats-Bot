4ddf356344534449d640db853a622fde0917ec63
hr951 <123540607+hr951@users.noreply.github.com>
2023-09-29 17:51:30 +0900

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -17,39 +17,45 @@ module.exports = {
     const thumbnail = interaction.client.user.displayAvatarURL();
       const username_2 = username;
       
-       const url = `https://scratchdb.lefty.one/v3/user/info/${username}/`;
+       const url = `https://scratchdb.lefty.one/v3/user/graph/${username}/followers?format=json`;
+      const url_2 = `https://scratchdb.lefty.one/v3/user/info/${username}/`;
 
   try {
     // ユーザー情報を取得するリクエストを送信
     const response = await fetch(url);
-
     // レスポンスをJSONに変換
     const json = await response.json();
-
+    const element = json[0];
     // ユーザー情報が存在しなければエラーメッセージを返す
     if (json.error) {
       interaction.reply({content: `Scratchのユーザー「${username_2}」は存在しません。`, ephemeral: true });
       return;
     }
+    
+    const response_2 = await fetch(url_2);
+    // レスポンスをJSONに変換
+    const json_2 = await response_2.json();
 
     // ユーザー情報から必要なデータを取得
-    const id = json.id;
-    const username = json.username;
-    var followers = json.statistics.followers;
-    var following = json.statistics.following;
-    var comments = json.statistics.comments;
-    var loves = json.statistics.loves;
-    var favorites = json.statistics.favorites;
-    var views = json.statistics.views;
-    var status = json.status;
-    var country = json.country;
-    var joined = json.joined;
-    console.log(`Username:${username}\nID:${id}\nFollowerCount:${followers}\nFollowingCount:${following}\nComenntCount:${comments}\nLoves:${loves}\nFavorites:${favorites}\nViews:${views}\nStatus:${status}\nCountry:${country}\njoined:${joined}`)
+    const id = json_2.id;
+    const username = json_2.username;
+    var followers = element.value;
+    //var following = json.statistics.following;
+    //var comments = json.statistics.comments;
+    //var loves = json.totals.loves;
+    //var favorites = json.totals.favorites;
+    //var views = json.totals.views;
+    //var debug = json.statistics;
+    var status = json_2.status;
+    var country = json_2.country;
+    var joined = json_2.joined;
+    //console.log(`Username:${username}\nID:${id}\nFollowerCount:${followers}\nFollowingCount:${following}\nComenntCount:${comments}\nLoves:${loves}\nFavorites:${favorites}\nViews:${views}\nStatus:${status}\nCountry:${country}\njoined:${joined}`)
+    console.log(`Username:${username}\nID:${id}\nFollowerCount:${followers}\nStatus:${status}\nCountry:${country}`)
     
     if(!followers){
       var followers = "0";
     } 
-    if(!following){
+    /*if(!following){
       var following = "0";
     } 
     if(!comments){
@@ -63,7 +69,7 @@ module.exports = {
     } 
     if(!views){
       var views = "0";
-    } 
+    } */
     var joined = joined.substr(0, joined.indexOf('T'));
     
     const embed = new EmbedBuilder()
@@ -86,21 +92,11 @@ module.exports = {
       inline: true
     },
     {
-      name: "Followers Count",
+      name: "Follower Count",
       value: `${followers}`,
       inline: true
     },
-    {
-      name: "Followings Count",
-      value: `${following}`,
-      inline: true
-    },
-    {
-      name: "Messages Count",
-      value: `${comments}`,
-      inline: true
-    },
-    {
+    /*{
       name: "Stars",
       value: `${favorites}`,
       inline: true
@@ -114,9 +110,10 @@ module.exports = {
       name: "Views",
       value: `${views}`,
       inline: true
-    },
+    },*/
   )
  .setThumbnail(`https://cdn2.scratch.mit.edu/get_image/user/${id}_90x90.png`)
+     //.setImage(`https://cdn2.scratch.mit.edu/get_image/user/${id}_90x90.png`)
   .setColor("#855DD7")
   .setFooter({
     text: "Made by Scratch Stats Bot",
