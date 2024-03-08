9fa88a689a031530036fa431d48deed6fbb8f999
hr951 <123540607+hr951@users.noreply.github.com>
2024-03-08 23:52:51 +0900

    Update code.js

--- code.js
+++ code.js
@@ -4,10 +4,11 @@ const path = require('node:path');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent
-  ]
-});
+  ]});
 
 const token = process.env['DISCORD_BOT_TOKEN']
 
@@ -60,6 +61,76 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+
+client.on('messageCreate', async message => {
+    if (!message.content.startsWith('!')) return
+    if (message.channel.id === "1215637785873227887"){
+    if (message.content === '!setting') {
+       const setting_status = await message.channel.messages.fetch("1215638473482960926");
+       const setting_stats = await message.channel.messages.fetch("1215639736169922560");
+       const setting_rank = await message.channel.messages.fetch("1215639808609751050");
+       const setting_explore = await message.channel.messages.fetch("1215639888473489429");
+       const setting_info = await message.channel.messages.fetch("1215639920694009916");
+       const setting_ping = await message.channel.messages.fetch("1215639950461243412");
+      const reply = await message.reply("設定が完了しました。");
+      setTimeout(() => {
+        message.delete();
+        reply.delete();
+      },5000);
+      if (setting_status.content.split("\n")[1].match(/@/)){
+        const setting_status_main = setting_status.content.split("\n")[1];
+        global.status = setting_status_main.slice(0, -1);
+      }else if (setting_status.content.split("\n")[2].match(/@/)){
+        const setting_status_main = setting_status.content.split("\n")[2];
+        global.status = setting_status_main.slice(0, -1);
+      }else if (setting_status.content.split("\n")[3].match(/@/)){
+        const setting_status_main = setting_status.content.split("\n")[3];
+        global.status = setting_status_main.slice(0, -1);
+      }else if (setting_status.content.split("\n")[4].match(/@/)){
+        const setting_status_main = setting_status.content.split("\n")[4];
+        global.status = setting_status_main.slice(0, -1);
+      }else if (setting_status.content.split("\n")[5].match(/@/)){
+        const setting_status_main = setting_status.content.split("\n")[5];
+        global.status = setting_status_main.slice(0, -1);
+      }
+      if (setting_stats.content.split("\n")[1].match(/@/)){
+        const setting_stats_main = setting_stats.content.split("\n")[1];
+        global.stats = setting_stats_main.slice(0, -1);
+      }else if (setting_stats.content.split("\n")[2].match(/@/)){
+        const setting_stats_main = setting_stats.content.split("\n")[2];
+        global.stats = setting_stats_main.slice(0, -1);
+      }
+      if (setting_rank.content.split("\n")[1].match(/@/)){
+        const setting_rank_main = setting_rank.content.split("\n")[1];
+        global.rank = setting_rank_main.slice(0, -1);
+      }else if (setting_rank.content.split("\n")[2].match(/@/)){
+        const setting_rank_main = setting_rank.content.split("\n")[2];
+        global.rank = setting_rank_main.slice(0, -1);
+      }
+      if (setting_explore.content.split("\n")[1].match(/@/)){
+        const setting_explore_main = setting_explore.content.split("\n")[1];
+        global.explore = setting_explore_main.slice(0, -1);
+      }else if (setting_explore.content.split("\n")[2].match(/@/)){
+        const setting_explore_main = setting_explore.content.split("\n")[2];
+        global.explore = setting_explore_main.slice(0, -1);
+      }
+      if (setting_info.content.split("\n")[1].match(/@/)){
+        const setting_info_main = setting_info.content.split("\n")[1];
+        global.info = setting_info_main.slice(0, -1);
+      }else if (setting_info.content.split("\n")[2].match(/@/)){
+        const setting_info_main = setting_info.content.split("\n")[2];
+        global.info = setting_info_main.slice(0, -1);
+      }
+      if (setting_ping.content.split("\n")[1].match(/@/)){
+        const setting_ping_main = setting_ping.content.split("\n")[1];
+        global.ping = setting_ping_main.slice(0, -1);
+      }else if (setting_ping.content.split("\n")[2].match(/@/)){
+        const setting_ping_main = setting_ping.content.split("\n")[2];
+        global.ping = setting_ping_main.slice(0, -1);
+      }
+      }}
+    });
+      
 
 
 client.login(token);
