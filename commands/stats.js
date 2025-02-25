0abe7a4a03125832b77aa4cae8955d93ca2a6778
hr951 <123540607+hr951@users.noreply.github.com>
2025-02-25 17:40:26 +0900

    フォロワー数の取得処理の変更

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fetch = require("node-fetch")
 
 module.exports = {
