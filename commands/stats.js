3e8fee0efc93fd06976d36686cf8578039111450
hr951 <123540607+hr951@users.noreply.github.com>
2025-06-25 19:50:00 +0900

    フォロワー・フォロー数取得大幅改善

--- commands/stats.js
+++ commands/stats.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const fetch = require("node-fetch");
 const fs = require("fs");
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,10 +30,10 @@ module.exports = {
     var count_following = 0;
     var followings = 0;
     var notfollower = 0;
-    
+
     const deleted_users = await fetch("https://raw.githubusercontent.com/hr951/Scratch-Stats-Bot/main/deleted_users.json");
     const deleted_users_json = await deleted_users.json();
-    
+
     function isUserDeleted(userId) {
       return deleted_users_json.some(user => user.id === userId);
     }
@@ -41,11 +43,11 @@ module.exports = {
     const url_5 = `https://scratch.mit.edu/site-api/users/all/${username_2}`;
 
     try {
-      
-     /*let url_7 = `https://scratch.mit.edu/users/${username_2}`;
-     const response_7 = await fetch(url_7);
-      console.log(response_7.status);*/
-      
+
+      /*let url_7 = `https://scratch.mit.edu/users/${username_2}`;
+      const response_7 = await fetch(url_7);
+       console.log(response_7.status);*/
+
       const response_2 = await fetch(url_2);
       const json_2 = await response_2.json();
       if (json_2.code === "NotFound" || json_2.code === "ResourceNotFound") {
@@ -55,11 +57,11 @@ module.exports = {
 
       const response_5 = await fetch(url_5);
       const json_5 = await response_5.json();
-      
+
       interaction.reply({ content: 'Now Loading...\n取得した情報はまもなく出力されます。\n※生成完了時メンションされます。', ephemeral: true });
       const mention = interaction.user.id;
-      
-      do{        
+
+      /*do{        
       const url = `https://api.scratch.mit.edu/users/${username_2}/following/?limit=40&offset=${count_following}`;
       
       const response = await fetch(url);
@@ -67,9 +69,39 @@ module.exports = {
       count_following = 40 + count_following;
       var followings = json.length + followings;
       global.json_length = json.length;
-      } while (global.json_length > 0);
-      
-      var followers_test = {};
+      } while (global.json_length > 0);*/
+
+      async function getFollowerCount(username, follow) {
+        try {
+          const response = await axios.get(`https://scratch.mit.edu/users/${username}/${follow}/`, {
+            headers: {
+              'User-Agent': 'Mozilla/5.0',
+            },
+          });
+
+          const dom = new JSDOM(response.data);
+          const document = dom.window.document;
+
+          // div.box-head の中のテキストを取得
+          const boxHead = document.querySelector('.box-head');
+          if (!boxHead) {
+            console.log('box-head が見つかりませんでした');
+            return;
+          }
+
+          const text = boxHead.textContent;
+          const match = text.match(/\((\d+)\)/); // フォロワー (853) から数字を抽出
+          if (match) {
+            return parseInt(match[1]);
+          } else {
+            return null;
+          }
+        } catch (err) {
+          console.error('エラー:', err.message);
+        }
+      }
+
+      /*var followers_test = {};
       var allFollowers = [];
       do{
       const url_3 = `https://api.scratch.mit.edu/users/${username_2}/followers/?limit=40&offset=${count_follower}`;
@@ -111,23 +143,23 @@ module.exports = {
         const attachment = new AttachmentBuilder('output.txt');
         member.send({ content: 'New Deleted Users List:', files: [attachment] });
       }
-      followers = allFollowers.length - notfollower;
-      
-      do{
-      const url_4 = `https://api.scratch.mit.edu/users/${username_2}/projects?limit=40&offset=${count_project}`;
+      followers = allFollowers.length - notfollower;*/
+
+      do {
+        const url_4 = `https://api.scratch.mit.edu/users/${username_2}/projects?limit=40&offset=${count_project}`;
         count_stats = 0;
-      
-      const response_4 = await fetch(url_4);
-      const json_4 = await response_4.json();
-      for(let i = 0; i < json_4.length ; i++) {
-      var views = json_4[count_stats].stats.views + views;
-      var stars = json_4[count_stats].stats.favorites + stars;
-      var hearts = json_4[count_stats].stats.loves + hearts;
-        count_stats ++;
-      }
-      count_project = 40 + count_project;
-      var projects = json_4.length + projects;
-      global.json_4_length = json_4.length;
+
+        const response_4 = await fetch(url_4);
+        const json_4 = await response_4.json();
+        for (let i = 0; i < json_4.length; i++) {
+          var views = json_4[count_stats].stats.views + views;
+          var stars = json_4[count_stats].stats.favorites + stars;
+          var hearts = json_4[count_stats].stats.loves + hearts;
+          count_stats++;
+        }
+        count_project = 40 + count_project;
+        var projects = json_4.length + projects;
+        global.json_4_length = json_4.length;
       } while (global.json_4_length > 0);
 
       // ユーザー情報から必要なデータを取得
@@ -146,29 +178,32 @@ module.exports = {
       var joined = `${year}/${month}/${day}`;
       var joined_time = `${hours}:${minutes}:${seconds}`;
       var pro_id = json_5.featured_project;
-      
+
+      followers = await getFollowerCount(username_2, "followers");
+      followings = await getFollowerCount(username_2, "following");
+
       const url_6 = `https://api.scratch.mit.edu/projects/${pro_id}`;
       const response_6 = await fetch(url_6);
-      if(response_6.status === 200){
-      const json_6 = await response_6.json();
-      var pro_title = json_6.title;
-      var pro_thumbnail = json_6.image;
-      }else{
+      if (response_6.status === 200) {
+        const json_6 = await response_6.json();
+        var pro_title = json_6.title;
+        var pro_thumbnail = json_6.image;
+      } else {
         var pro_title = "不明 / NotFound";
         var pro_thumbnail = "https://cdn2.scratch.mit.edu/get_image/project/1042518320_480x360.png";
         var pro_id = "1042518320";
       }
-      
+
       const response_8 = await fetch(`https://scratch.mit.edu/users/${username}`);
-      if(response_8.status === 404){
+      if (response_8.status === 404) {
         var title = `${username}'s Stats (Deleted User)`;
       } else {
         var title = `${username}'s Stats`
       }
-      
-      if(json_2.scratchteam){
+
+      if (json_2.scratchteam) {
         var status = "Scratch Team";
-      }else{
+      } else {
         var status = "Scratcher";
       }
 
@@ -234,7 +269,7 @@ module.exports = {
           iconURL: thumbnail,
         })
         .setTimestamp();
-      
+
       await interaction.channel.send({ content: `<@${mention}> __@${username_2}__の情報を生成しました。`, embeds: [embed] });
       //await interaction.editReply({ embeds: [embed] })
 
