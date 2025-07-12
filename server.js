0a132df0090981d714a8a5cf07f34a209d7f5cfe
hr951 <123540607+hr951@users.noreply.github.com>
2025-07-12 18:38:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const http = require("http");
-const querystring = require("node:querystring");
+const express = require("express");
+const app = express();
 
-//GASでwakeさせること。
+const PORT = process.env.PORT || 3000; // ← ここ大事！
 
-http
-  .createServer(function(req, res) {
-    if (req.method == "POST") {
-      var data = "";
-      req.on("data", function(chunk) {
-        data += chunk;
-      });
-      req.on("end", function() {
-        if (!data) {
-          res.end("No post data");
-          return;
-        }
-        var dataObject = querystring.parse(data);
-        console.log("post:" + dataObject.type);
-        if (dataObject.type == "wake") {
-          console.log("Woke up in post");
-          res.end();
-          return;
-        }
-        res.end();
-      });
-    } else if (req.method == "GET") {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Discord Bot is Oprateing!");
-    }
-  })
-  .listen(3000);
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
 
-if (process.env.DISCORD_BOT_TOKEN == undefined || process.env.DISCORD_BOT_TOKEN == "") {
-  console.log("DISCORD_BOT_TOKENを設定してください。");
-  process.exit(0);
-}
-
-require("./code.js")
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
